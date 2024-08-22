d44a04a51616b5201b7ce78277c35cf1ffec403a
OcAdriann <ocadriannnc20@gmail.com>
2024-08-22 13:47:38 -0400

    Game is rendered

--- War-card-game/src/App.jsx
+++ War-card-game/src/App.jsx
@@ -1,19 +1,21 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Game from "./pages/Game"
-import Result from "./pages/Result"
-import NotFound from "./pages/NotFound"
+import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+import Game from './pages/Game'; // Assuming Game component is here
+// import Home from './components/Home.jsx'; // Assuming Home component is here
+// import Result from './components/Result.jsx'; // Assuming Result component is here
+// import NotFound from './components/NotFound.jsx'; // Assuming NotFound component is here
 
 function App() {
   return (
     <>
-      <Routes>
+    <Game />
+      {/* <Routes> */}
         {/* <Route path="/" element={<Dashboard />}></Route> */}
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/game" element={<Game />}></Route>
-        <Route path="/result" element={<Result />}></Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        {/* <Route path="/home" element={<Home />}></Route> */}
+        {/* <Route path="/game" element={<Game />}></Route> */}
+        {/* <Route path="/result" element={<Result />}></Route> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      {/* </Routes> */}
     </>
   );
 }
--- War-card-game/src/main.jsx
+++ War-card-game/src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { GameProvider } from './components/GameProvider.jsx'
-// import './index.css'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import GameProvider from './components/GameProvider.jsx';
+// import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <GameProvider>
       <App />
     </GameProvider>
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
+
